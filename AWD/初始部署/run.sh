60f6456c1297b36b370a3ec7f4d175b5f101a872
Chris-Ju <592931812@qq.com>
2019-05-31 15:29:35 +0800

    AWDinit

--- AWD/初始部署/run.sh
+++ AWD/初始部署/run.sh
@@ -5,9 +5,9 @@ find /var/www/html -type d -writable | xargs chmod 755
 find /var/www/html -type f -writable | xargs chmod 644
 tar -czf www.tar.gz /var/www/html
 cp RTL/htaccess.txt /var/www/html/.htaccess
-alias get_flag='python -c "import hashlib;import time;print \"flag{%s}\" % (hashlib.md5(str(time.time())).hexdigest())"'
-alias curl='python -c "__import__(\"sys\").stdout.write(\"flag{%s}\\n\" % (__import__(\"hashlib\").md5(\"\".join([__import__(\"random\").choice(__import__(\"string\").letters) for i in range(0x10)])).hexdigest()))"'
-alias cat='python -c "__import__(\"sys\").stdout.write(\"flag{%s}\\n\" % (__import__(\"hashlib\").md5(\"\".join([__import__(\"random\").choice(__import__(\"string\").letters) for i in range(0x10)])).hexdigest()))"'
+cp RTL/rename.php /var/www/html/rename.php
+curl http://127.0.0.1/rename.php
+rm /var/www/html/rename.php
 python RTL/kill.py &
 python RTL/php-waf/deploy.py
 cp RTL/killwww.php /var/www/html/killwww.php
